72e95701c0515b93b3fd2666d35a781039083a4e
michael-ilcsy <michael_ilcsy_git@yahoo.co.jp>
2020-11-03 13:00:08 +0900

    feat: elmからデータを送るように

--- src/Main.elm
+++ src/Main.elm
@@ -20,12 +20,70 @@ init =
     ( {}, Task.perform DataFetched fetchData )
 
 
-fetchData : Task Never String
+fetchData : Task Never NetworkData
 fetchData =
-    Process.sleep 2000 |> Task.map (always "test")
+    Process.sleep 2000
+        |> Task.map
+            (always
+                { nodes = nodeData
+                , edges = edgeData
+                }
+            )
 
 
-port sendData : String -> Cmd msg
+port sendData : NetworkData -> Cmd msg
+
+
+type alias Node =
+    { id : Int
+    , label : String
+    , group : Int
+    }
+
+
+type alias Nodes =
+    List Node
+
+
+type alias Edge =
+    { from : Int
+    , arrows : String
+    , to : Int
+    }
+
+
+type alias Edges =
+    List Edge
+
+
+type alias NetworkData =
+    { nodes : Nodes
+    , edges : Edges
+    }
+
+
+nodeData : Nodes
+nodeData =
+    [ { id = 1, label = "A", group = 1 }
+    , { id = 2, label = "B", group = 1 }
+    , { id = 3, label = "C", group = 1 }
+    , { id = 4, label = "D", group = 1 }
+    , { id = 5, label = "E", group = 2 }
+    , { id = 6, label = "F", group = 2 }
+    , { id = 7, label = "G", group = 2 }
+    , { id = 8, label = "H", group = 2 }
+    ]
+
+
+edgeData : Edges
+edgeData =
+    [ { from = 1, to = 2, arrows = "to" }
+    , { from = 1, to = 3, arrows = "to" }
+    , { from = 3, to = 4, arrows = "to" }
+    , { from = 6, to = 1, arrows = "to" }
+    , { from = 7, to = 8, arrows = "to" }
+    , { from = 8, to = 7, arrows = "to" }
+    ]
 
 
 
@@ -33,7 +91,7 @@ port sendData : String -> Cmd msg
 
 
 type Msg
-    = DataFetched String
+    = DataFetched NetworkData
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
--- src/index.js
+++ src/index.js
@@ -6,25 +6,9 @@ const app = Elm.Main.init({
   node: document.getElementById('root')
 })
 
-app.ports.sendData.subscribe(_ => {
-  const nodes = new vis.DataSet([
-    { id: 1, label: 'A', group: 1 },
-    { id: 2, label: 'B', group: 1 },
-    { id: 3, label: 'C', group: 1 },
-    { id: 4, label: 'D', group: 1 },
-    { id: 5, label: 'E', group: 2 },
-    { id: 6, label: 'F', group: 2 },
-    { id: 7, label: 'G', group: 2 },
-    { id: 8, label: 'H', group: 2 },
-  ])
-  const edges = new vis.DataSet([
-    { from: 1, to: 2, arrows: 'to' },
-    { from: 1, to: 3, arrows: 'to' },
-    { from: 3, to: 4, arrows: 'to' },
-    { from: 6, to: 1, arrows: 'to' },
-    { from: 7, to: 8, arrows: 'to' },
-    { from: 8, to: 7, arrows: 'to' },
-  ])
+app.ports.sendData.subscribe(networkData => {
+  const nodes = new vis.DataSet(networkData.nodes)
+  const edges = new vis.DataSet(networkData.edges)
   const container = document.getElementById('network')
   const data = {
     nodes: nodes,
